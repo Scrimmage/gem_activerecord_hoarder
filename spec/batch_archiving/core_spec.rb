2ca2271a4316b6eaf4fb610bdd966788212cefbe
M E <matthias.engh@gmail.com>
2017-09-29 12:19:40 -0500

    increment from base by starting TDD

---
+++ spec/batch_archiving/core_spec.rb
@@ -0,0 +1,11 @@
+
+
+describe 'BatchArchiving::Core' do
+  context 'included and instantiated' do
+    describe 'archive_batch' do
+      it "does something useful" do
+        expect(true) to be false
+      end
+    end
+  end
+end
