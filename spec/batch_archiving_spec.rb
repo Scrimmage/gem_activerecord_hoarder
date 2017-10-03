2265c83e2e9a225a7b270a0c60e7fa0e7b34afd0
M E <matthias.engh@gmail.com>
2017-10-03 15:34:44 -0500

    specs for record purge ready

--- spec/batch_archiving_spec.rb
+++ spec/batch_archiving_spec.rb
@@ -36,54 +36,50 @@ RSpec.describe BatchArchiving do
     end
 
     context "with records only in current week" do
-      let!(:archivable_records) {
-        create_list(
+      before do
+        @archivable_records = create_list(
           :examples_in_range,
           20,
           deleted: true,
           start_time: Time.now.getutc.beginning_of_week,
           end_time: (Date.today - 1).to_time(:utc).end_of_day
         )
-      }
-      let!(:non_archivable_records) {
-        create_list(
+        @non_archivable_records = create_list(
           :examples_on_date,
           4,
           deleted: true,
           records_date: Date.today
         )
-      }
+      end
 
       it "ignores current day" do
-        expect(Example.unscoped.to_a).to include(*non_archivable_records)
+        expect(Example.unscoped.to_a).to include(*@non_archivable_records)
       end
 
       it "archives previous days" do
-        expect(Example.unscoped.to_a).not_to include(*archivable_records)
+        expect(Example.unscoped.to_a).not_to include(*@archivable_records)
       end
     end
 
     context "with records in multiple weeks, non-deleted records mixed in and trailing" do
-      let!(:archivable_records) {
-        create_list(
+      before do
+        @archivable_records = create_list(
           :examples_in_range,
           20,
-          deleted: :true,
+          deleted: true,
           end_time: (1.week.ago.getutc.beginning_of_week + 2.days).end_of_day,
           start_time: 1.week.ago.getutc.beginning_of_week
         ) + create_list(
           :examples_in_range,
           20,
-          deleted: :true,
+          deleted: true,
           end_time: 1.week.ago.getutc.end_of_week,
           start_time: 1.week.ago.getutc.beginning_of_week + 4.days
         )
-      }
-      let!(:non_archivable_records) {
-        create_list(
+        @non_archivable_records = create_list(
           :examples_on_date,
           4,
-          deleted: :true,
+          deleted: true,
           records_date: (1.week.ago.getutc.beginning_of_week + 3.days).to_date,
         ) + create_list(
           :examples_on_date,
@@ -91,19 +87,31 @@ RSpec.describe BatchArchiving do
           deleted: false,
           records_date: (1.week.ago.getutc.beginning_of_week + 3.days).to_date
         )
-      }
-      let!(:out_of_range_records) {}
+        @out_of_range_records = create_list(
+          :examples_in_range,
+          2,
+          deleted: true,
+          end_time: Time.now,
+          start_time: Time.now.getutc.beginning_of_week
+        ) + create_list(
+          :examples_in_range,
+          2,
+          deleted: false,
+          end_time: Time.now,
+          start_time: Time.now.getutc.beginning_of_week
+        )
+      end
 
       it "skips records from days with active records" do
-        expect(Example.unscoped.to_a).to include(non_archivable_records)
+        expect(Example.unscoped.to_a).to include(*@non_archivable_records)
       end
 
       it "archives one week of fully deleted records" do
-        expect(Example.unscoped.to_a).not_to include(archivable_records)
+        expect(Example.unscoped.to_a).not_to include(*@archivable_records)
       end
 
       it "stops after one week" do
-        expect(Example.unscoped.to_a).to include(out_of_range_records)
+        expect(Example.unscoped.to_a).to include(*@out_of_range_records)
       end
     end
   end
