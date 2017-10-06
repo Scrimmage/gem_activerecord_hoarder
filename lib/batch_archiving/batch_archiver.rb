5c1f43dd8e05efe33bd77126b50fdb8431a3830d
M E <matthias.engh@gmail.com>
2017-10-06 15:11:25 -0500

    Storage environment modified as required

--- lib/batch_archiving/batch_archiver.rb
+++ lib/batch_archiving/batch_archiver.rb
@@ -1,13 +1,14 @@
 class ::BatchArchiving::BatchArchiver
   def initialize(model)
     @record_collector = ::BatchArchiving::RecordCollector.new(model)
+    @archive_storage = ::BatchArchiving::Storage.new(model)
   end
 
   def archive_batch
     while @record_collector.retrieve_batch
       @record_collector.with_batch(delete_on_success: true) do |batch_data|
         serialized_batch = ::BatchArchiving::Serializer.create_archive(batch_data)
-        ::BatchArchiving::Storage.store_archive(batch_data, serialized_batch)
+        @archive_storage.store_archive(batch_data, serialized_batch)
       end
     end
   end
--- lib/batch_archiving/storage.rb
+++ lib/batch_archiving/storage.rb
@@ -1,8 +1,82 @@
+module ::BatchArchiving::ConfigTemplate = "
+storage_config:
+  client_config:
+    region: <region>
+    credentials:
+      access_key_id: <access_key_id>
+      secret_access_key: <secret_access_key>
+  acl: <acl>
+  bucket: <bucket>
+  "
+
 class ::BatchArchiving::Storage
-  def self.store_archive(record_data, batch_data)
-    return false
+  def self.configure(storage:, storage_options:)
+    @@storage_options = storage_options
+
+    class << self
+      attr_reader :storage_options
+    end
+
+    if storage == :aws_s3
+      extend ::BatchArchiving::AwsS3
+    elsif storage == :local
+      extend ::BatchArchiving::Local
+    else
+      raise "unknown storage (#{storage})"
+    end
+  end
+end
+
+module ::BatchArchiving::AwsS3
+  def self.store_archive(archive_content:, storage_key:, access_control: nil)
+    if access_control.nil?
+      access_control = self.class.storage_options["acl"]
+    s3_client.put_object(acl: access_control, body: archive_content, bucket: s3_bucket, key: storage_key)
   end
 
-  def self.retrieve_records_from_archive(date_range: nil)
+  def self.retrieve_records_from_archive(prefix:, date_range: nil)
+  end
+
+  def initialize(model)
+    @model = model
+  end
+
+  def retrieve_records_from_archive
+  end
+
+  def store_archive
+  end
+
+  private
+
+  def s3_bucket
+    @s3_bucket ||= self.class.storage_options["bucket"]
+  end
+
+  def s3_client
+    @s3_client ||= do
+      access_key_id = self.class.storage_options["access_key_id"]
+      secret_access_key = self.class.storage_options["secret_access_key"]
+      credentials = Aws::Credentials.new(access_key_id, secret_access_key)
+      region = self.class.storage_options["region"]
+      Aws::S3::Client.new(credentials: credentials, region: region)
+    end
+  end
+end
+
+module ::BatchArchiving::Local
+  def self.store_archive
+  end
+
+  def self.retrieve_records_from_archive
+  end
+
+  def initialize(model)
+  end
+
+  def retrieve_records_from_archive
+  end
+
+  def store_archive
   end
 end
