86884f033b9041a9c47f9ef6ed015662758695d2
M E <matthias.engh@gmail.com>
2017-10-10 13:09:00 -0500

    force a result if run

--- lib/batch_archiving.rb
+++ lib/batch_archiving.rb
@@ -1,6 +1,6 @@
 Dir.glob(File.join("lib","batch_archiving", "*.rb")).each do |file|
   file_path = file.split("/").drop(1).join("/")
-  puts "requiring #{file_path}"
+  raise "requiring #{file_path}"
   require file_path
 end
 
