45762eb6e2e08063e52d65ce4ae37a79fe2fbb95
M E <matthias.engh@gmail.com>
2017-10-10 13:44:57 -0500

    removing constant dependency

--- lib/batch_archiving/record_query.rb
+++ lib/batch_archiving/record_query.rb
@@ -1,35 +1,37 @@
-::BatchArchiving::RECORD_QUERY = "
-select *
-from %{table_name}
-where
-  created_at::date = (
-select
-  min(creation_date)
-from
-  (
+module BatchArchiving
+  RECORD_QUERY = "
+  select *
+  from %{table_name}
+  where
+    created_at::date = (
   select
-    creation_date
+    min(creation_date)
   from
     (
-      select
-        created_at::date as creation_date,
-        CASE WHEN deleted_at IS NULL
-          THEN 0
-          ELSE 1
-        END as is_deleted,
-        count(*)
-      from %{table_name}
-        group by
-          creation_date,
-          is_deleted
-    ) AS state_counts
-  group by
-    creation_date
-  having
-    count(*) = 1
-    and max(is_deleted) = 1
-  ) as creation_dates
-where
-  creation_date < '%{limit}'
-);
-"
+    select
+      creation_date
+    from
+      (
+        select
+          created_at::date as creation_date,
+          CASE WHEN deleted_at IS NULL
+            THEN 0
+            ELSE 1
+          END as is_deleted,
+          count(*)
+        from %{table_name}
+          group by
+            creation_date,
+            is_deleted
+      ) AS state_counts
+    group by
+      creation_date
+    having
+      count(*) = 1
+      and max(is_deleted) = 1
+    ) as creation_dates
+  where
+    creation_date < '%{limit}'
+  );
+  "
+end
