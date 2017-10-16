68cc2f1694c125fdd8828935b598be122d27a6a1
M E <matthias.engh@gmail.com>
2017-10-16 10:55:20 -0500

    fixure is now adding examples on load

--- bin/example/fixture.rb
+++ bin/example/fixture.rb
@@ -2,4 +2,8 @@ def create_examples(count, start: 0, deleted: true)
   (1..count).each do |n|
     Example.create(content: "example: #{n}", created_at: (n+start).hours.ago, deleted_at: Time.now)
   end
+  nil
 end
+
+create_examples(3, start: 0, deleted: true)
+create_examples(3, start: 0.5, deleted: true)
