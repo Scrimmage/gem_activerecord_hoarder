ea86bb75bd36404e55a1bffa139aa1ed276333b3
M E <matthias.engh@gmail.com>
2017-10-24 12:12:22 -0500

    configure testing database

--- spec/support/1_active_record_configuration.rb
+++ spec/support/1_active_record_configuration.rb
@@ -1,6 +1,3 @@
-database_name = "batch_archiving_rspec.sqlite3"
 
-ActiveRecord::Base.establish_connection(
-  adapter: :sqlite3,
-  database: database_name
-)
+
+ActiveRecord::Base.establish_connection(YAML.load_file("config/dbspec_rspec.yml"))
