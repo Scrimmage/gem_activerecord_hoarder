ad5c1307fabff642a581edeb1ca99bcc8653aa84
M E <matthias.engh@gmail.com>
2017-10-25 12:20:10 -0500

    avoiding namespace problems

--- lib/batch_archiving/aws_s3_storage.rb
+++ lib/batch_archiving/aws_s3_storage.rb
@@ -1,50 +1,52 @@
-class ::BatchArchiving::AwsS3
-  DEFAULT_ACL = "private"
-  OPTION_CONTENT_ACCESS = "acl"
-  OPTION_SUB_DIR = "bucket_sub_dir"
-  OPTION_BUCKET = "bucket"
-  OPTION_ACCESS_KEY_ID = "access_key_id"
-  OPTIONS_SECRET_ACCESS_KEY = "secret_access_key"
-  OPTION_REGION = "region"
+module BatchArchiving
+  class AwsS3
+    DEFAULT_ACL = "private"
+    OPTION_CONTENT_ACCESS = "acl"
+    OPTION_SUB_DIR = "bucket_sub_dir"
+    OPTION_BUCKET = "bucket"
+    OPTION_ACCESS_KEY_ID = "access_key_id"
+    OPTIONS_SECRET_ACCESS_KEY = "secret_access_key"
+    OPTION_REGION = "region"
 
-  attr_reader :storage_options
+    attr_reader :storage_options
 
-  def initialize(model_class, storage_options)
-    @storage_options = storage_options
+    def initialize(model_class, storage_options)
+      @storage_options = storage_options
 
-    record_name = model_class.table_name
+      record_name = model_class.table_name
 
-    if storage_options[OPTION_SUB_DIR].blank?
-      @key_prefix = record_name
-    else
-      @key_prefix = File.join(storage_options[OPTION_SUB_DIR], record_name)
+      if storage_options[OPTION_SUB_DIR].blank?
+        @key_prefix = record_name
+      else
+        @key_prefix = File.join(storage_options[OPTION_SUB_DIR], record_name)
+      end
     end
-  end
 
-  def store_archive(content:, file_type:, key_sequence:, options: {})
-    storage_key = File.join(@key_prefix, key_sequence) + '.' + file_type.to_s
+    def store_archive(content:, file_type:, key_sequence:, options: {})
+      storage_key = File.join(@key_prefix, key_sequence) + '.' + file_type.to_s
 
-    s3_client.put_object(bucket: s3_bucket, body: content, key: storage_key, acl: s3_acl)
-    true
-  end
+      s3_client.put_object(bucket: s3_bucket, body: content, key: storage_key, acl: s3_acl)
+      true
+    end
 
-  private
+    private
 
-  def s3_acl
-    storage_options[OPTION_CONTENT_ACCESS] || DEFAULT_ACL
-  end
+    def s3_acl
+      storage_options[OPTION_CONTENT_ACCESS] || DEFAULT_ACL
+    end
 
-  def s3_bucket
-    storage_options[OPTION_BUCKET]
-  end
+    def s3_bucket
+      storage_options[OPTION_BUCKET]
+    end
 
-  def s3_client
-    @s3_client ||= begin
-      access_key_id = storage_options[OPTION_ACCESS_KEY_ID]
-      secret_access_key = storage_options[OPTIONS_SECRET_ACCESS_KEY]
-      credentials = Aws::Credentials.new(access_key_id, secret_access_key)
-      region = storage_options[OPTION_REGION]
-      Aws::S3::Client.new(credentials: credentials, region: region)
+    def s3_client
+      @s3_client ||= begin
+        access_key_id = storage_options[OPTION_ACCESS_KEY_ID]
+        secret_access_key = storage_options[OPTIONS_SECRET_ACCESS_KEY]
+        credentials = Aws::Credentials.new(access_key_id, secret_access_key)
+        region = storage_options[OPTION_REGION]
+        Aws::S3::Client.new(credentials: credentials, region: region)
+      end
     end
   end
 end
