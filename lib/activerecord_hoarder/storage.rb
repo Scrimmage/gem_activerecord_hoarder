80f50f6dacf8dbcf2dd203b7ed7b77c71ea9a443
M E <matthias.engh@gmail.com>
2017-11-16 13:35:43 -0600

    pointless 'private' statement removed
    no effect on class methods

--- lib/activerecord_hoarder/storage.rb
+++ lib/activerecord_hoarder/storage.rb
@@ -6,22 +6,22 @@ class ::ActiverecordHoarder::Storage
 
     self.storage_options = storage_options
     self.storage = storage
+
     self
   end
 
-  private
+  def self.check_configured
+    raise ::ActiverecordHoarder::StorageError.new("storage needs to be configured") unless is_configured?
+
+  end
+
+  def self.is_configured?
+    storage.present? && storage_options.is_a?(Hash)
+  end
 
   def self.new(table_name, storage_override: nil, storage_options_override: {})
     self.check_configured
     storage_class = ::ActiverecordHoarder::Storages.retrieve(storage_override || storage)
     storage_class.new(table_name, storage_options.merge(storage_options_override))
-  end
-
-  def self.check_configured
-    raise ::ActiverecordHoarder::StorageError.new("storage needs to be configured") unless is_configured?
-  end
-
-  def self.is_configured?
-    storage.present? && storage_options.is_a?(Hash)
   end
 end
