b23b374e4ab5902e48c549b5da9354e8c7bfbb05
M E <matthias.engh@gmail.com>
2017-11-16 14:14:48 -0600

    reorder function definitions

--- lib/activerecord_hoarder/storage.rb
+++ lib/activerecord_hoarder/storage.rb
@@ -1,6 +1,17 @@
 class ::ActiverecordHoarder::Storage
   class_attribute :storage, :storage_options
 
+  def self.new(table_name, storage_override: nil, storage_options_override: {})
+    self.check_configured
+    storage_class = ::ActiverecordHoarder::Storages.retrieve(storage_override || storage)
+    storage_class.new(table_name, storage_options.merge(storage_options_override))
+  end
+
+  def self.check_configured
+    raise ::ActiverecordHoarder::StorageError.new("storage needs to be configured") unless is_configured?
+
+  end
+
   def self.configure(storage:, storage_options:)
     ::ActiverecordHoarder::Storages.is_valid_storage?(storage)
 
@@ -10,18 +21,7 @@ class ::ActiverecordHoarder::Storage
     self
   end
 
-  def self.check_configured
-    raise ::ActiverecordHoarder::StorageError.new("storage needs to be configured") unless is_configured?
-
-  end
-
   def self.is_configured?
     storage.present? && storage_options.is_a?(Hash)
-  end
-
-  def self.new(table_name, storage_override: nil, storage_options_override: {})
-    self.check_configured
-    storage_class = ::ActiverecordHoarder::Storages.retrieve(storage_override || storage)
-    storage_class.new(table_name, storage_options.merge(storage_options_override))
   end
 end
